fb19bd612debc6d43f5b7ac9d55af42b9ab64b11
kolpi12 <kolpi12@naver.com>
2019-11-15 20:58:16 +0900

    지도상에 표현하는 유동인구 값의 기준 시간을 12시 고정에서 시간 슬라이더의 값에 따라 바뀌도록 변경
    /index.html
    추가 - 기준 시간값을 정하는 id가 time_slider인 input:range 요소
    
    /css/map.css
    추가 - h3 태그는 이제 inline 속성
    
    /script/map.js
    추가 - 지도상에 표현하는 유동인구 값의 기준 시간을 12시 고정에서 시간 슬라이더의 값에 따라 바뀌도록 변경
    수정 - 지도상에 표현하는 유동인구 값이 바뀔 때 색상도 같이 바뀜
    수정 - 코드 리팩토링

--- script/map.js
+++ script/map.js
@@ -1,95 +1,118 @@
 class Dong{
     constructor(code, name) {
-        this.dong_code = code;
-        this.dong_name = name;
-        this.hour_pop = {};
-        for (let index = 0; index < 24; index++) {
-            this.hour_pop[index] = 0;
+        this.dongHjCode = code;
+        this.dongName = name;
+        this.hourPop = {};
+        for(let index = 0; index < 24; index++) {
+            this.hourPop[index] = 0;
         }
+    }
+    
+    plusHourPop(time, value) {
+        this.hourPop[time] += value;
+    }
+
+    getHourPop(time) {
+        return this.hourPop[time];
     }
 }
 
 function documentInit(boxId, mapJson, mapObj) {
-    // 행정동 데이터 받아오기
+    // 각종 변수 초기화
     var dong = {};
-    function dongInit() {
-        var dongCode = d3.json("json/dong_cd.json").then(function(d) {
-            for(let index = 0; index < d.length; index++) {
-                dong[d[index].H_DNG_CD] = new Dong(d[index].H_DNG_CD, d[index].H_DNG_NM);
-            }
-        });
-    }
-    
-    // 시내 유동인구 데이터 받아오기
-    var color = d3.scaleThreshold()
-    function floatingPopInit() {
-        var floatingPop = d3.csv("data/floating_pop/INNER_PEOPLE_20191101.csv").then(function(d) {
-            let max = 0;
-            for(let index = 0; index < d.length; index++) {
-                let data = d[index];
-                data["총생활인구수"] = Math.round(data["총생활인구수"]);
-                dong[data['행정동코드']]["hour_pop"][+data['시간대구분']] += +(data['총생활인구수']);
-                if(max < data['총생활인구수']) {
-                    max = data['총생활인구수'];
-                }
-                // TODO: 총생활인구수 합계 최댓값 구하기 -> 컬러 스킴 지정
-            }
-            for (const k in dong) {
-                if (dong.hasOwnProperty(k)) {
-                    const element = dong[k];
-                    console.log(element['hour_pop'])
-                }
-            }
-            color = d3.scaleThreshold()
-                .domain([1000,2000,3000,4000,5000,10000,20000,50000])
-                .range(d3.schemeYlOrBr[9]);
-            mapSvg.selectAll("path")
-                .attr("fill", function(d) { return color(dong[d.properties.EMD_HJ_CD]['hour_pop'][12]); });
-        });
-        // TODO: 일정 시간대/성별/연령대/거주지/행정구역 별로 유동인구 추출
-        // TODO: 2개 이상의 유동인구 데이터 비교 분석
-    }
-
-    // 맵 만들기
+    dongInit();
+    console.log(dong);
     const WIDTH = 750,
     HEIGHT = 610;
-    var svg = d3.select(boxId).append("svg")
-    .attr("width", WIDTH)
-    .attr('height', HEIGHT);
-    var mapSvg = svg.append("g").attr("id", "map");
-    var guName = document.querySelector("#gu_nm > h3");
-    var dongName = document.querySelector("#dong_nm > h3");
-    var hjCode = document.querySelector("#hj_cd > h3");
-    function setTooltip(gu, dong, code) {
+    var svg = d3.select(boxId).append('svg')
+        .attr('width', WIDTH)
+        .attr('height', HEIGHT);
+    var mapSvg = svg.append('g').attr('id', 'map');
+    var guName = document.querySelector('#gu_nm > h3');
+    var dongName = document.querySelector('#dong_nm > h3');
+    var hjCode = document.querySelector('#hj_cd > h3');
+    var pop = document.querySelector('#pop > h3');
+    var timeSlider = document.querySelector('#time_slider');
+    setTooltip('종로구', '청운효자동', 11110515, 12345);
+    d3.select('#time_slider').on('input', function() {
+        changeBaseTime(+this.value);
+    })
+    var color = d3.scaleThreshold()
+        .domain([2000,4000,6000,8000,10000,20000,50000])
+        .range(d3.schemeYlOrBr[9]);
+
+    // 편의 함수
+    function pad(n, width) {
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+    }
+
+    function setTooltip(gu, dong, code, popVal) {
         guName.innerText = gu;
         dongName.innerText = dong;
-        hj_cd.innerText = code;
+        hjCode.innerText = code;
+        pop.innerText = popVal;
     }
+
+    function changeFeatureColor() {
+        mapSvg.selectAll('path')
+            .attr('fill', function(d) { return color(dong[d.properties.EMD_HJ_CD].getHourPop(timeSlider.value)); });
+    }
+
+    function changeBaseTime(time) {
+        d3.select('#time_slider').property('value', time);
+        changeFeatureColor();
+        pop.innerText = dong[hjCode.innerText].getHourPop(timeSlider.value);
+        document.querySelector('#time').innerText = `${pad(timeSlider.value, 2)}시`;
+    }
+
+    // 동 초기화
+    function dongInit() {
+        d3.json("json/dong_cd.json").then(function(d) {
+            for(let index = 0; index < d.length; index++) {
+                let data = d[index];
+                dong[data.H_DNG_CD] = new Dong(data.H_DNG_CD, data.H_DNG_NM);
+            }
+            changeBaseTime(12);
+        });
+    }
+
+    // 동 유동인구 초기화
+    function floatingPopInit(map) {
+        d3.csv('data/floating_pop/INNER_PEOPLE_20191101.csv').then(function(d) {
+            for (let index = 0; index < d.length; index++) {
+                let data = d[index];
+                data['총생활인구수'] = Math.round(data['총생활인구수']);
+                dong[data['행정동코드']].plusHourPop(+data['시간대구분'], +data['총생활인구수']);
+            }
+            changeFeatureColor();
+        });
+    }
+
+    // 동 지도 그리기
     function mapInit(mapJson, mapObj) {
-        var mapFeature = d3.json(mapJson).then(function(d) {
+        d3.json(mapJson).then(function(d) {
             let mapData = topojson.feature(d, d.objects[mapObj]);
             let center = d3.geoCentroid(mapData);
-            center[0] -= 0.015;
-            center[1] += 0.015;
+            center[0] -= 0.015; center[1] += 0.015;
             let scale = 100000;
             let projection = d3.geoMercator().translate([WIDTH/2, HEIGHT/2])
                 .scale(scale).center(center);
             let path = d3.geoPath().projection(projection);
 
-            mapSvg.selectAll("path")
+            mapSvg.selectAll('path')
                 .data(mapData.features)
-                .enter().append("path")
-                .attr("d", path)
-                .on("mouseover", function(d) {
-                    setTooltip(d.properties.SIG_KOR_LN, d.properties.ADM_DR_NM, dong[d.properties.EMD_HJ_CD]['hour_pop'][12]);
-                })
-                // .on("mouseover", function(){ // 마우스 호버 시 z-index 높이기 위함
-                //     d3.select(this).raise();
-                // });
-                // mouseout
+                .enter().append('path')
+                .attr('d', path)
+                .on('mouseover', function(d) {
+                    let prop = d.properties;
+                    setTooltip(prop.SIG_KOR_LN, prop.ADM_DR_NM, prop.EMD_HJ_CD, dong[prop.EMD_HJ_CD].getHourPop(timeSlider.value));
+                });
         });
     }
-    dongInit();
-    floatingPopInit();
+
     mapInit(mapJson, mapObj);
+    floatingPopInit(mapSvg);
 }
+
+// test code
